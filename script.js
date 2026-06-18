9cb58f69b9e3c653945420d5a1971cef3604313b
Shegzy221 <addjoseph33@gmail.com>
2026-06-18 20:29:47 +0100

    Modify image carousel functionality
    Updated image array and adjusted interval timing.

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const hero = document.querySelector(".hero");
 
 const images = [
+
 "https://images.unsplash.com/photo-1492144534655-ae79c964c9d3?w=1600",
+
 "https://images.unsplash.com/photo-1503376780353-7e6692767b70?w=1600",
-"https://images.unsplash.com/photo-1489824904134-891ab64532f1?w=1600"
+
+"https://images.unsplash.com/photo-1552519507-da3b142c6e3d?w=1600"
+
 ];
 
 let current = 0;
 
 setInterval(() => {
+
 current++;
-if (current >= images.length) {
+
+if(current >= images.length){
 current = 0;
 }
 
 hero.style.backgroundImage =
-`linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.6)),url(${images[current]})`;
+`linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.6)),
+url(${images[current]})`;
 
-}, 5000);
-function topFunction(){
-window.scrollTo({
-top:0,
-behavior:"smooth"
-});
-}
+},4000);
