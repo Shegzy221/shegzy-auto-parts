4238bed464b59f49360db81c4c6f3204ca62eb76
Shegzy221 <addjoseph33@gmail.com>
2026-06-18 16:22:59 +0100

    Change background style with gradient for hero section
    Updated the image background style to include a gradient overlay.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const hero = document.querySelector(".hero");
 
 const images = [
+"https://images.unsplash.com/photo-1492144534655-ae79c964c9d3?w=1600",
 "https://images.unsplash.com/photo-1503376780353-7e6692767b70?w=1600",
-"https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?w=1600",
 "https://images.unsplash.com/photo-1489824904134-891ab64532f1?w=1600"
 ];
 
@@ -10,11 +10,11 @@ let current = 0;
 
 setInterval(() => {
 current++;
-if(current >= images.length){
+if (current >= images.length) {
 current = 0;
 }
 
-hero.style.background =
-`url(${images[current]}) center/cover`;
+hero.style.backgroundImage =
+`linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.6)),url(${images[current]})`;
 
-},5000);
+}, 5000);
